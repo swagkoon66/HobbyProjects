df9363c2e89fd310a195c5ba05cc1e858ecd13b1
PongraweeThepchai <165445055+swagkoon66@users.noreply.github.com>
2024-12-25 15:15:21 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE *FILEloc;
+
+int main () {
+    char messages[100];
+    if ((FILEloc = fopen("text.txt","r")) == NULL){
+        printf("NULL FILEloc program terminated");
+        getchar();
+        exit(0);
+    }
+    FILEloc = fopen("text.txt","r");
+    printf("\n\n\t\t\t\tTHE INBOUND MESSAGE IS\n");
+    while (fgets(messages,100,FILEloc) != NULL){
+        printf("%s",messages);
+    }
+}
